229bd6ff02297bdee3e16637da235d6f213bb83e
Prudhvi Sai Koppaka <prudhvisaikoppaka99@gmail.com>
2026-03-29 15:34:51 +0530

    vpc-module-test-terraform

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ module "vpc" {
   /* project             = "roboshop"
   environment         = "dev"
   public_subnet_cidrs = ["10.0.1.0/24", "10.0.2.0/24"] */
-  
+
   project               = var.project
   environment           = var.environment
   public_subnet_cidrs   = var.public_subnet_cidrs
